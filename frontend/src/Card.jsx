8b0f47e7cb2feb814a7d58a810c2c925feccf648
Saja Elfaki <saja@meta.com>
2024-06-17 15:56:45 -0700

    creation of kudo card

---
+++ frontend/src/Card.jsx
@@ -0,0 +1,17 @@
+import '.Card.css'
+
+function Card() {
+  return (
+    <div className="card">
+        <p>Text Meesage</p>
+        <img src="..public/G5398.png" alt=''/>
+        <p>Card Author</p>
+        <div className='card-buttons'>
+            <button>Like</button>
+            <button>Delete</button>
+        </div>
+    </div>
+  )
+}
+
+export default Card;
